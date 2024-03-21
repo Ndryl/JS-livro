22eb5e4a0eb7a406cbd609f6717ff912cf155919
Ndryl <hendrylmr@outlook.com>
2024-03-21 10:33:46 -0300

    feat: Exercício 3 referente ao capítulo 5

---
+++ CAP5/exercicio3_CAP5.js
@@ -0,0 +1,8 @@
+var rua = ['Rua 1', 'Rua 2', 'Rua 3', 'Rua 4'];
+for (var percorreRua of rua){
+    if(percorreRua === 'Rua 3'){
+        console.log(percorreRua);
+        break;
+    }
+    console.log(percorreRua);
+}
