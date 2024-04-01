2777d329b6c31079315d6efdc59d20a341fd7e6d
Ndryl <hendrylmr@outlook.com>
2024-04-01 11:58:15 -0300

    Feat: questão de expansão com operadores

---
+++ CAP14/exercicio_4_CAP14.js
@@ -0,0 +1,2 @@
+var argumentos = [1,2,3];
+console.log(...argumentos);
---
+++ CAP14/exercicio_5_CAP14.js
@@ -0,0 +1,8 @@
+const equipeMarketing = ['Joana', 'Marcela', 'Bruna'];
+const equipeComercial = ['Talita', 'Luisa', 'Vitória'];
+
+const timeCompleto = [...equipeComercial, ...equipeMarketing];
+
+for(let consol of timeCompleto){
+    console.log(consol);
+}
