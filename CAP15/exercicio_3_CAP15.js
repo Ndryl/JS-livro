47eb4ecfc38fb9860b8483f2bccae6cae6fdd0e2
Ndryl <hendrylmr@outlook.com>
2024-04-02 07:22:29 -0300

    FEAT: Exercícios relacionados a desestruturamento de dados

---
+++ CAP15/exercicio_3_CAP15.js
@@ -0,0 +1,7 @@
+const usuario = {
+    nome: 'Toreto',
+    email: 'velozesefuriososparasempre@gmail.com'
+  }
+  let {nome} = usuario;
+  let {email} = usuario;
+  console.log(`${nome}\n${email}`);
